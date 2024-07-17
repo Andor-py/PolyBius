0576dd0b8be68a317c10afd4b392e9490b19240c
Andor <146471594+Andor-py@users.noreply.github.com>
2024-07-17 20:09:50 +0200

    Add files via upload

---
+++ config_selfbot.py
@@ -0,0 +1,78 @@
+from utils import rpc
+#######################
+#  selfbot            #
+#        basic        #
+#         config ^^   #
+#######################
+
+# en: SelfBot name
+# fr: Nom du SelfBot
+selfbot_name = "PolyBius" # Tip: Don't use the "Selfbot" word into your selfbot name, most of servers blacklist this word with AutoMod
+
+# en: Account Token.
+# fr: Token du compte.
+token = ""
+
+# en: Commands prefix.
+# fr: Prefix des commandes.
+prefix = "*"
+
+# fr: Langue.
+# en: Language.
+lang = "en" # fr / en
+
+# fr: Activer/Désactiver les logs de discord (ex: Connected to gateway , Rate Limited etc..).
+# en: Toggle discord logs (like: Connected to gateway, Rate Limited etc...).
+discord_log = True
+
+# fr: Default Nitro Sniper mode. (True=On, False=Off)
+# fr: Mode du Nitro Sniper par défaut. (True=On, False=Off)
+nitro_sniper = False
+
+# en: Commands delay of delete.
+# fr: Délai de supression des commandes.
+deltime = 3
+########################
+
+
+#######################
+#  good               #
+#        person       #
+#######################
+
+# en: Default paramter for Good Person.
+# fr: Paramètre par défaut de Good Person.
+good_person = False
+
+# en: Good Person badwords.
+# fr: Mot interdit pour Good Person.
+badwords = ["fuck", "shit", "pute", "connard", "connasse", "conasse", "nigg", "bitch", "kys", "fdp", "ntm", "tg"]
+
+# en: Good Person "good words".
+# fr: Mot "bon" pour Good Person.
+good_person_list = [
+        "GeForce RTX 4000",
+        "🍗",
+        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.",
+        "AMD Ryzen™ 9 7900",
+        "Intel Core is very good",
+        "🐈",
+        "🍟",
+        "yipeeeeeeeee",
+        "😍",
+        "🌠",
+        "u r beautiful",
+        "you are all very intelligent",
+        "excuse me"
+        ]
+########################
+########################
+
+#######################
+#  raid               #
+#        config       #
+#######################
+# en: Ban reason (for &banall).
+# fr: Raison du banissement (pour &banall).
+ban_reason = "ezzed by Andor lol."
+kick_reason = "ezzed by Andor lol."
